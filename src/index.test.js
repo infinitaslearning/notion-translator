77f4d5f40a8d6d54629138dea7ec9d10009c94a3
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2023-02-01 17:27:35 +0100

    Add multi-select test case

--- src/index.test.js
+++ src/index.test.js
@@ -14,8 +14,25 @@ test('complete input should succeed with default inputs', () => {
   process.env.INPUT_FIELD_TO_TRANSLATE_STATUS = 'Translated Status'
   process.env.INPUT_FIELD_TO_TRANSLATE_RESULT = 'Translated'
   process.env.INPUT_FIELD_TO_TRANSLATE_LANGUAGE = 'Language'
+
+  const ip = path.join(__dirname, 'index.js')
+  const options = {
+    env: process.env
+  }
+  const result = cp.execSync(`node ${ip}`, options).toString()
+  expect(result).toBeDefined()
+})
+
+test('complete input should succeed with default inputs on multi select fields', () => {
+  process.env.INPUT_NOTION_TOKEN = process.env.NOTION_TOKEN
+  process.env.INPUT_DATABASE = '3ea38518219c45dc8051c5c70b9de5ca'
+  process.env.INPUT_TRANSLATION_ENGINE = 'google'
   process.env.INPUT_DEFAULT_LANGUAGE_FROM = 'nl'
   process.env.INPUT_DEFAULT_LANGUAGE_TO = 'en'
+  process.env.INPUT_FIELD_TO_TRANSLATE = 'Tags'
+  process.env.INPUT_FIELD_TO_TRANSLATE_STATUS = 'Translated tag status'
+  process.env.INPUT_FIELD_TO_TRANSLATE_RESULT = 'Translated tags'
+  process.env.INPUT_FIELD_TO_TRANSLATE_LANGUAGE = 'Language tags'
 
   const ip = path.join(__dirname, 'index.js')
   const options = {
